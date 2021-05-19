e5bb45d6548249ecc0579ffbe26a9e7af0a99847
yooinsun <yellowranger95@gmail.com>
2021-05-19 14:32:53 +0900

    test upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "test",
+        "excerpt":"무야호 이것은 테스트      이것은 테스트 입니다   ` rbenv versions # 버전 확인 gem install jekyll # Jekyll 설치하기 Gem install budler `  ","categories": [],
+        "tags": [],
+        "url": "/test/",
+        "teaser": null
+      }]
